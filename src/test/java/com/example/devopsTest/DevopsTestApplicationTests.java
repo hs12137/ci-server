14e370a7b0bc265dbe4a2b2c0c509eff5b5d32f4
HuiSeokJeong <tobysug123@gmail.com>
2024-05-21 12:03:11 +0900

    test

--- src/test/java/com/example/devopsTest/DevopsTestApplicationTests.java
+++ src/test/java/com/example/devopsTest/DevopsTestApplicationTests.java
@@ -2,13 +2,25 @@ package com.example.devopsTest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.AssertionErrors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DevopsTestApplicationTests {
+	@Value("${spring.datasource.username}")
+	private String username;
+	@Value("${spring.datasource.url}")
+	private String url;
 
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void dbIsH2(){
+		assertEquals("sa", username);
+		assertEquals("jdbc:h2:mem:test", url);
+	}
 }
